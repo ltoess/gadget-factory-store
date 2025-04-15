b9ac3c080c1bb52cd6d8196933246d5b384f807f
ltoess <lukeoess@gmail.com>
2025-04-14 20:52:53 -0500

    set up basic class structure

---
+++ src/edu/ilstu/Gadget.java
@@ -0,0 +1,23 @@
+package edu.ilstu;
+
+public class Gadget {
+
+	private int price; 
+	private int id;
+	
+	
+	public Gadget(int materialPrice) {
+		this.price = price;
+	}
+	
+	public int getPrice() {
+		return price;
+	}
+	
+	public String toString() {
+		return "Price: " + price + " ID: " + id; 
+	}
+	
+	
+	
+}
