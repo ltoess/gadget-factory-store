79c7dd3d67a5cbf6299e0f3da707dd7ed6e5ee68
ltoess <lukeoess@gmail.com>
2025-04-15 13:54:24 -0500

    added random objects to driver and order

--- src/edu/ilstu/Gadget.java
+++ src/edu/ilstu/Gadget.java
@@ -1,14 +1,17 @@
 package edu.ilstu;
- 
+
+import java.util.Random;
+
 public class Gadget {
 
+	private Random rand = new Random();
 	private int price; // the price is determined by the material price, which fluctuates in between [10,15] monthly  
 	private int id; // a unique identifier to each gadget produced 
 	
 	
 	public Gadget(int materialPrice) {
 		// generates the price and id for the current gadget object
-		this.price = materialPrice;
+		this.price = materialPrice + rand.nextInt(5,11);
 	}
 	
 	public int getPrice() {
--- src/edu/ilstu/GadgetFactoryStore.java
+++ src/edu/ilstu/GadgetFactoryStore.java
@@ -1,6 +1,8 @@
 package edu.ilstu;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Random;
 import java.util.Stack;
 
 public class GadgetFactoryStore {
@@ -8,16 +10,24 @@ public class GadgetFactoryStore {
 	
 	public static void main(String[] args) {
 		LocalDate date = LocalDate.of(2025, 4, 1);
-		System.out.println("                Welcome to the Gadget Factory Store Simulation System");
+		Random rand = new Random();
 		
-		Stack<Gadget> gStack = new Stack<>();
+		System.out.println("			Welcome to the Gadget Factory Store Simulation System");
+		Stack<List<Gadget>> batches = new Stack<>();
+	
 		
-		// start of loop
+	
 		
 		for (int i = 0; i < 100; i++ ) {	
-			
 			System.out.println("Today's date: " + date.toString());
-		
+			int materialPrice = rand.nextInt(10,16);
+			Stack<Gadget> gStack = new Stack<>();
+			
+			
+			
+			
+			
+			
 			date = date.plusDays(1);
 		}
 		
